245fbe850c7b272764985d7e34e365a51f7a28fe
lenni <lenni31@neuf.fr>
2016-10-07 10:58:42 +0200

    final app.js quatrequart clicker

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-
-
 $(document).ready(function(){
 var i = 0;
 $("img").click(function(){
-
 $("span").text(++i);
-
 if (i % 20 === 0){
 	alert("trouver une activité plus productive !!!!")
 }
-
-console.log(i)
-
-
 });
-
-
 });
