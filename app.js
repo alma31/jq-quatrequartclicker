96111683ca0659e7a0ed7eff40cc9ea508d659cb
lenni <lenni31@neuf.fr>
2016-10-07 09:34:29 +0200

    final clickers

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+
+
 $(document).ready(function(){
-	//votre code
+var i = 0;
+$("img").click(function(){
+
+$("span").text(++i);
+
+if (i % 20 === 0){
+	alert("trouver une activité plus productive !!!!")
+}
+
+console.log(i)
+
+
+});
 
 
 });
